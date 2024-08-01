8521510631396b1ac951d09900769874c300af3b
StefanYves <stefannisipeanu02@gmail.com>
2024-08-01 20:19:21 +0300

    .

--- db/pool.js
+++ db/pool.js
@@ -1,7 +1,8 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+const connectionString = process.env.DATABASE_URL;
+
 module.exports = new Pool({
-  connectionString:
-    "postgres://minimessageboard-top-main-db-056fc097540a84396:rd5ejJqbFrDPhXTn3KqyNp5R4ud65V@user-prod-us-east-2-1.cluster-cfi5vnucvv3w.us-east-2.rds.amazonaws.com:5432/minimessageboard-top-main-db-056fc097540a84396",
+  connectionString: connectionString,
 });
